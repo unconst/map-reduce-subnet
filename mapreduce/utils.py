b72aa917537a4ca35883d42bc0896b6a51dc9cf9
dream-well <dreamwell506@outlook.com>
2023-11-30 15:22:56 +0000

    fix check_processes function

--- mapreduce/utils.py
+++ mapreduce/utils.py
@@ -156,10 +156,11 @@ def check_processes(processes, miner_status = None):
                 if miner_status[miner_uid]['status'] == 'benchmarking':
                     miner_status[miner_uid]['status'] = 'unavailable'
                     miner_status[miner_uid]['retry'] = miner_status[miner_uid].get('retry', 0) + 1
+                    bt.logging.warning(f"Benchmark for Miner {miner_uid} Retry: {miner_status[miner_uid]['retry']}")
                     if miner_status[miner_uid]['retry'] > 3:
                         miner_status[miner_uid] = 'failed'
             if 'miners' in processes[key]:
-                for uid in processes[key]['miners']:
+                for (uid, _) in processes[key]['miners']:
                     if miner_status[int(uid)]['status'] == 'working':
                         miner_status[int(uid)]['status'] = 'available'
             del processes[key]
@@ -253,7 +254,7 @@ def get_unused_port(start_port, end_port):
 Retrieves the amount of available bandwidth from the free memory.
 """
 def calc_bandwidth_from_memory(free_memory: int):
-    return max(int((free_memory - 300 * 1024 * 1024) / 2), 0)
+    return max(int((free_memory - 500 * 1024 * 1024) / 2), 0)
 
 """
 Retrieves the amount of free memory in the system.
