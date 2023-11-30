de9c0917ecf291861170e5249b1df9cdd61420ce
dream-well <dreamwell506@outlook.com>
2023-11-30 14:40:17 +0000

    setup monitored barrie which fails with detailed crash information

--- neurons/dist_validator.py
+++ neurons/dist_validator.py
@@ -86,8 +86,9 @@ class Validator:
                     backend='gloo',
                     rank=self.rank,
                     world_size=self.world_size,
-                    timeout=timedelta(seconds=30)
+                    timeout=timedelta(seconds=15)
                 )
+                dist.monitored_barrier(timeout=timedelta(seconds=15))
 
                 bt.logging.info(f"Process group initialized.")
                 # Init groups
