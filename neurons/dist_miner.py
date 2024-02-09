13cd2a1e044c1af8dff795cabc301e3b35eb3d19
dream-well <dreamwell506@outlook.com>
2024-02-09 13:31:14 +0000

    ver: 1.9.4: add timeout for creating groups

--- neurons/dist_miner.py
+++ neurons/dist_miner.py
@@ -50,11 +50,11 @@ class MinerDist:
     # Initialize the groups for the miner
     # This method sets up the groups for the miner.
     def initialize_groups(self):
-        self.peers = dist.new_group(ranks=list(range(1, self.peer_count + 1)))
-        self.miners = dist.new_group(ranks=list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)))
-        self.peer_miners = [dist.new_group(ranks=[peer_rank] + list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1))) for peer_rank in range(1, self.peer_count + 1)]
-        self.miner_peers = [dist.new_group(ranks=[miner_rank] + list(range(1, self.peer_count + 1))) for miner_rank in range(self.peer_count + 1, self.peer_count + self.peer_count + 1)]
-        self.validator_rank1 = dist.new_group(ranks=(0, 1))
+        self.peers = dist.new_group(ranks=list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=10))
+        self.miners = dist.new_group(ranks=list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=10))
+        self.peer_miners = [dist.new_group(ranks=[peer_rank] + list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=10)) for peer_rank in range(1, self.peer_count + 1)]
+        self.miner_peers = [dist.new_group(ranks=[miner_rank] + list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=10)) for miner_rank in range(self.peer_count + 1, self.peer_count + self.peer_count + 1)]
+        self.validator_rank1 = dist.new_group(ranks=(0, 1), timeout=timedelta(seconds=15))
         bt.logging.info('Groups initialized')
 
     # Scatter data to miners
--- neurons/dist_validator.py
+++ neurons/dist_validator.py
@@ -97,10 +97,10 @@ class Validator:
     # Initialize the groups for the validator.
     def init_groups(self):
         bt.logging.info("Creating groups")
-        self.peers = dist.new_group(ranks=list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=5))
-        self.miners_group = dist.new_group(ranks=list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=5))
-        self.peer_miners = [dist.new_group(ranks=[peer_rank] + list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=5)) for peer_rank in range(1, self.peer_count + 1)]
-        self.miner_peers = [dist.new_group(ranks=[miner_rank] + list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=5)) for miner_rank in range(self.peer_count + 1, self.peer_count + self.peer_count + 1)]
+        self.peers = dist.new_group(ranks=list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=10))
+        self.miners_group = dist.new_group(ranks=list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=10))
+        self.peer_miners = [dist.new_group(ranks=[peer_rank] + list(range(self.peer_count + 1, self.peer_count + self.peer_count + 1)), timeout=timedelta(seconds=10)) for peer_rank in range(1, self.peer_count + 1)]
+        self.miner_peers = [dist.new_group(ranks=[miner_rank] + list(range(1, self.peer_count + 1)), timeout=timedelta(seconds=10)) for miner_rank in range(self.peer_count + 1, self.peer_count + self.peer_count + 1)]
         self.validator_rank1 = dist.new_group(ranks=(0, 1), timeout=timedelta(seconds=15))
         bt.logging.info('Groups created')
 
